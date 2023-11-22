4ce74ab800d2ed83e386e093d5ea18ddb9a0198d
Ruttala Gangadhara rao <136541746+Gangadhara2024@users.noreply.github.com>
2023-11-22 16:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const button = document.getElementById("btn");
+const select = document.getElementById("colorSelect");
+
+button.addEventListener("click", (e) => {
+	if(e.target === "SELECT"){
+		console.log(e.target);
+	}
+})
